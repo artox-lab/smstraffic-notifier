10804094a6bd0e76c65c28324de9f700553edfa9
Maxim Petrovich <m.petrovich@artox.com>
2021-07-08 11:49:21 +0300

    Add compatibility with new version of API. Add failover host

--- SmsTrafficTransport.php
+++ SmsTrafficTransport.php
@@ -7,18 +7,22 @@
 namespace  ArtoxLab\Component\Notifier\Bridge\SmsTraffic;
 
 use Symfony\Component\Notifier\Exception\LogicException;
+use Symfony\Component\Notifier\Exception\RuntimeException;
 use Symfony\Component\Notifier\Exception\TransportException;
 use Symfony\Component\Notifier\Message\MessageInterface;
 use Symfony\Component\Notifier\Message\SentMessage;
 use Symfony\Component\Notifier\Transport\AbstractTransport;
 use Symfony\Component\Notifier\Message\SmsMessage;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class SmsTrafficTransport extends AbstractTransport
 {
 
-    public const HOST = 'sds.smstraffic.ru';
+    public const HOST = 'api.smstraffic.ru';
+    public const FAILOVER_HOST = 'api2.smstraffic.ru';
 
     /**
      * Login
@@ -77,30 +81,78 @@ class SmsTrafficTransport extends AbstractTransport
             throw new LogicException(sprintf('The "%s" transport only supports instances of "%s" (instance of "%s" given).', __CLASS__, SmsMessage::class, get_debug_type($message)));
         }
 
-        $endpoint = sprintf('https://%s/smartdelivery-in/multi.php', $this->getEndpoint());
-        $response = $this->client->request(
-            'POST',
-            $endpoint,
-            [
-                'json' => [
-                    'login'      => $this->login,
-                    'password'   => $this->password,
-                    'phones'     => $message->getPhone(),
-                    'message'    => $message->getSubject(),
-                    'rus'        => 0,
-                    'originator' => $this->from,
-                ],
-            ]
-        );
-
-        if (200 !== $response->getStatusCode()) {
-            throw new TransportException(
-                'Unable to send the SMS',
-                $response
-            );
+        try {
+            $response = $this->makeRequest("https://{$this->getEndpoint()}/multi.php", $message);
+            $this->validateResponse($response);
+        } catch (TransportExceptionInterface $exception) {
+            try {
+                $response = $this->makeRequest("https://" . self::FAILOVER_HOST . "/multi.php", $message);
+                $this->validateResponse($response);
+            } catch (TransportExceptionInterface $exception) {
+                throw new RuntimeException(
+                    'Unable to send the SMS: ' . $exception->getMessage()
+                );
+            }
         }
 
         return new SentMessage($message, (string) $this);
+    }
+
+    /**
+     * Make Api request
+     *
+     * @param string $endpoint Api endpoint
+     * @param MessageInterface $message Message
+     *
+     * @return ResponseInterface
+     * @throws TransportExceptionInterface
+     */
+    private function makeRequest(string $endpoint, MessageInterface $message): ResponseInterface
+    {
+        return $this->client->request(
+            'POST',
+            $endpoint,
+            [
+                'headers' => [
+                    'Content-Type' => 'application/x-www-form-urlencoded',
+                ],
+                'body' => [
+                    'login'      => $this->login,
+                    'password'   => $this->password,
+                    'originator' => $this->from,
+                    'phones'     => $message->getPhone(),
+                    'message'    => $message->getSubject(),
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Validate response
+     *
+     * @param ResponseInterface $response Response
+     *
+     * @return void
+     * @throws TransportExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     */
+    private function validateResponse(ResponseInterface $response): void
+    {
+        if (200 !== $response->getStatusCode()) {
+            throw new TransportException('Unable to send the SMS', $response);
+        }
+
+        if (false === strpos($response->getContent(), '<result>OK</result>')){
+            $message = 'Unable to send the SMS';
+
+            if (preg_match('|<code>(\d+)</code>|s', $response->getContent(), $match)) {
+                $message .= ". Code $match[1]";
+            }
+
+            throw new TransportException($message, $response);
+        }
     }
 
     /**
@@ -124,5 +176,4 @@ class SmsTrafficTransport extends AbstractTransport
     {
         return sprintf('smstraffic://%s?from=%s', $this->getEndpoint(), $this->from);
     }
-
 }
